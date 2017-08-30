ce63223e80fc8beb18f4579e3676f7bb052c584a
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:27:54 +0530

    Create assignment 2 problem 5.cpp

---
+++ assignment 2 problem 5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	float x,y;
+	cout<<"enter x and y";
+	cin>>x>>y;
+	cout<<"the value of x^y is "<<pow(x,y);
+
+	return 0;
+}
