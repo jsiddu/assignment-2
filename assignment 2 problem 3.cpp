b5d09867f2c49815d8a1bac845f26a098b146243
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:19:38 +0530

    Create assignment 2 problem 3.cpp

---
+++ assignment 2 problem 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a;
+	cout<<"enter the temperature in farenheit";
+	cin>>a;
+	cout<<"the temperature in celcius is"<<(a-32)*5/9;
+	// your code goes here
+	return 0;
+}
