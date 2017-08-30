620f54ac3238f45c70f589f65eaa9fc7a2cca4f3
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:44:30 +0530

    Create assignment 2 problem 10.cpp

---
+++ assignment 2 problem 10.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	float x,y,z,a,b;
+	cout<<"enter the marks";
+	cin>>x>>y>>z>>a>>b;
+	cout<<"the total is  "<<x+y+z+a+b;
+	cout<<"the average is"<<(x+y+z+a+b)/5;
+	cout<<"the percentage is"<<(x+y+z+a+b)*20;
+
+	return 0;
+}
