fd7928b66b3e6524a1e8f8948602c04f250d0c2d
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:47:45 +0530

    Create assignment 2 problem 11.cpp

---
+++ assignment 2 problem 11.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	float x,y,z,a,b;
+	cout<<"enter P,T,R";
+	cin>>x>>y>>z;
+	cout<<"the simple interest is  "<<x*y*z;
+
+
+	return 0;
+}
