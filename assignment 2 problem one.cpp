a80018502e293b5bdeafeff5f3dd422474ae4f52
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:00:19 +0530

    Create assignment 2 problem one.cpp

---
+++ assignment 2 problem one.cpp
@@ -0,0 +1,12 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {
+    	int a;
+    	cout<<"enter the length in centimeter";
+    	cin>>a;
+    	cout<<"the length in meters is "<<a/100;
+    	cout<<"the length in kilometer is "<<a/100000;
+    	// your code goes here
+    	return 0;
+    }
