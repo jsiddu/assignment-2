c9388382d4198149c0aebe91c71a7bc939509530
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:37:49 +0530

    Create assignment 2 problem 9.cpp

---
+++ assignment 2 problem 9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	float x,y;
+	cout<<"enter the length of the side of the triangle";
+	cin>>x;
+	cout<<"the area of the equilateral triangle is  "<<1.732*x/4*x;
+
+	return 0;
+}
