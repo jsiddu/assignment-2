8f7d74b11a61f3989d28160640e217a4d1ee5c23
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:24:05 +0530

    Create assignment 2 problem 4.cpp

---
+++ assignment 2 problem 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a;
+	cout<<"enter the number of days";
+	cin>>a;
+	cout<<"the number of years is "<<a/365;
+	cout<<"the number of weeks is "<<a/7;
+	return 0;
+}
