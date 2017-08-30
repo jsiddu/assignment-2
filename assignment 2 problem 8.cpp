39fb3eeaf0e0238bbc0a7fc1076b3de0577f3fa7
jsiddu <31274868+jsiddu@users.noreply.github.com>
2017-08-30 15:34:06 +0530

    Create assignment 2 problem 8.cpp

---
+++ assignment 2 problem 8.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main() {
+	float x,y;
+	cout<<"enter the values of two angles of the triangle";
+	cin>>x>>y;
+	cout<<"the value of the third angle is  "<<180-(x+y);
+
+	return 0;
+}
